e2b6cea3e82b81fd0ff6c93476c4f13a4133d7d2
Gabr19 <sannagabriele19@gmail.com>
2023-02-03 20:31:04 +0100

    fine footer

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -60,47 +60,47 @@ export default{
                     <div>
                         DC COMICS
                         <ul>
-                            <li></li>
-                            <li></li>
-                            <li></li>
-                            <li></li>
-                            <li></li>
-                            <li></li>
-                            <li></li>
+                            <li>Characters</li>
+                            <li>Comics</li>
+                            <li>Movies</li>
+                            <li>TV</li>
+                            <li>Games</li>
+                            <li>Videos</li>
+                            <li>News</li>
                         </ul>
                     </div>
                     <div>
                         SHOP
                         <ul>
-                            <li></li>
-                            <li></li>
+                            <li>Shop DC</li>
+                            <li>Shop DC Collectibles</li>
                         </ul>
                     </div>
                 </div>
                 <div>
                     DC
                     <ul>
-                        <li></li>
-                        <li></li>
-                        <li></li>
-                        <li></li>
-                        <li></li>
-                        <li></li>
-                        <li></li>
-                        <li></li>
-                        <li></li>
-                        <li></li>
-                        <li></li>
+                        <li>Terms Of Use</li>
+                        <li>Privacy Policy (New)</li>
+                        <li>Ad Choices</li>
+                        <li>Advertising</li>
+                        <li>Jobs</li>
+                        <li>Subscriptions</li>
+                        <li>Talent Workshops</li>
+                        <li>CPSC Certificates</li>
+                        <li>Ratings</li>
+                        <li>Shop Help</li>
+                        <li>Contact Us</li>
                     </ul>
                 </div>
                 <div>
                     SITES
                     <ul>
-                        <li></li>
-                        <li></li>
-                        <li></li>
-                        <li></li>
-                        <li></li>
+                        <li>DC</li>
+                        <li>MAD Magazine</li>
+                        <li>DC Kids</li>
+                        <li>DC Universe</li>
+                        <li>DC Power Visa</li>
                     </ul>
                 </div>
             </div>
@@ -109,16 +109,26 @@ export default{
         <div class="down-footer">
             <div class="container-center-down">
                 <div id="sign-up">
-
+                    SIGN-UP NOW!
                 </div>
                 <div id="down-list">
                     <ul>
-                        <li></li>
-                        <li></li>
-                        <li></li>
-                        <li></li>
-                        <li></li>
-                        <li></li>
+                        <li id="follow-us">FOLLOW US</li>
+                        <li>
+                            <font-awesome-icon icon="fa-brands fa-square-facebook" />
+                        </li>
+                        <li>
+                            <font-awesome-icon icon="fa-brands fa-square-twitter" />
+                        </li>
+                        <li>
+                            <font-awesome-icon icon="fa-brands fa-square-youtube" />
+                        </li>
+                        <li>
+                            <font-awesome-icon icon="fa-brands fa-square-pinterest" />                        
+                        </li>
+                        <li>
+                            <font-awesome-icon icon="fa-solid fa-location-dot" />
+                        </li>
                     </ul>
                 </div>
             </div>
@@ -135,10 +145,10 @@ export default{
          width: 100%;
     };
     .up-footer{
-            height: 100px;
-            background-color: #0282f9;
-            display: flex;
-            justify-content: center;
+        height: 100px;
+        background-color: #0282f9;
+        display: flex;
+        justify-content: center;
         };
 
     .container-center-up{
@@ -178,10 +188,13 @@ export default{
 
 
     .middle-footer{
-        height: 250px;
+        height: 60vh;
         background-color: gray;
         display: flex;
         justify-content: center;
+        color: white;
+        background-image: url(../assets/img/footer-bg.jpg);
+        
     };
 
     .container-center-middle{
@@ -194,6 +207,14 @@ export default{
     .container-center-middle > div{
         margin-right: 20px;
     };
+
+    .container-center-middle li{
+        color: #7a8071;
+        margin: 5px 0;
+        list-style: none;
+    };
+
+
 
     .down-footer{
         height: 100px;
@@ -210,9 +231,15 @@ export default{
     };
 
     .container-center-down #sign-up{
-        background-color: #0282f9;
+        border: 2px solid #0282f9;
         height: 50px;
-        width: 100px;
+        min-width: 100px;
+        color: white;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        padding: 0 10px;
+
     };
 
 
@@ -225,8 +252,23 @@ export default{
         list-style: none;
         margin: 0 10px;
         height: 50px;
-        width: 50px;
-        border: 1px solid black;
-        background-color: #0282f9
+        min-width: 50px;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+
     };
+
+    .container-center-down #down-list ul > li > * {
+        font-size: 30px;
+        color: white
+        
+    };
+
+    .container-center-down #down-list #follow-us {
+        color: #0282f9;
+        font-weight: bold;
+
+    }
+
 </style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,29 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+
+/* import the fontawesome core */
+import { library } from '@fortawesome/fontawesome-svg-core'
+
+/* import font awesome icon component */
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+/* import specific icons */
+
+import { faSquareFacebook, faSquarePinterest, faSquareTwitter, faSquareYoutube } from '@fortawesome/free-brands-svg-icons'
+import { faLocationDot } from '@fortawesome/free-solid-svg-icons'
+
+
+/* add icons to the library */
+library.add(
+     
+    faSquarePinterest,
+    faSquareFacebook,
+    faSquareTwitter,
+    faSquareYoutube,
+    faLocationDot
+    )
+
+createApp(App)
+    .component('font-awesome-icon', FontAwesomeIcon)
+    .mount('#app')
